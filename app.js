722b058ea146386f90d6390874b63ff97e6153a2
Wenhao (Leo) Liang <36321327+foxhatleo@users.noreply.github.com>
2023-11-19 19:11:42 -0800

    Fix apk static

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("apk", express.static(path.join(__dirname, "data", "apk")));
 
+app.use("/apk", express.static(path.join(__dirname, "data", "apk")));
 app.use("/", indexRouter);
 
 module.exports = app;
