0709aa753700e1936a83ee0292bee62ab0354992
Wenhao (Leo) Liang <36321327+foxhatleo@users.noreply.github.com>
2024-02-25 00:14:20 -0800

    Change update file

--- bin/update.js
+++ bin/update.js
@@ -37,30 +37,29 @@ async function downloadApk(codename, friendlyName, url, localPath = undefined) {
     fs.copyFileSync(apkPath, destApkPath);
     console.log(`Copied ${codename}.`);
 
-    let finalFilename = destApkFilename;
-
-    if (typeof url === "object") {
-        finalFilename = { "arm64-v8a": finalFilename };
-        for (const arch of Object.keys(url)) {
-            if (arch === "arm64-v8a") continue;
-            console.log(`Downloading ${codename} for arch ${arch}...`);
-            const archURL = url[arch];
-            // eslint-disable-next-line no-await-in-loop
-            const apkRes = await fetch(archURL);
-            const destApkFilename = `${codename}_${apkVersion}_${arch}.apk`;
-            const destApkPath = path.join(destApkFolderPath, destApkFilename);
-            const apkFs = fs.createWriteStream(destApkPath, { flags: "w" });
-            // eslint-disable-next-line no-await-in-loop
-            await finished(Readable.fromWeb(apkRes.body).pipe(apkFs));
-            finalFilename[arch] = destApkFilename;
-            console.log(`Downloaded ${codename} for arch ${arch}.`);
-        }
-    }
+    const finalFilename = destApkFilename;
+    // if (typeof url === "object") {
+    //     finalFilename = { "arm64-v8a": finalFilename };
+    //     for (const arch of Object.keys(url)) {
+    //         if (arch === "arm64-v8a") continue;
+    //         console.log(`Downloading ${codename} for arch ${arch}...`);
+    //         const archURL = url[arch];
+    //         // eslint-disable-next-line no-await-in-loop
+    //         const apkRes = await fetch(archURL);
+    //         const destApkFilename = `${codename}_${apkVersion}_${arch}.apk`;
+    //         const destApkPath = path.join(destApkFolderPath, destApkFilename);
+    //         const apkFs = fs.createWriteStream(destApkPath, { flags: "w" });
+    //         // eslint-disable-next-line no-await-in-loop
+    //         await finished(Readable.fromWeb(apkRes.body).pipe(apkFs));
+    //         finalFilename[arch] = destApkFilename;
+    //         console.log(`Downloaded ${codename} for arch ${arch}.`);
+    //     }
+    // }
 
     return {
-        version: apkVersion,
-        package: apkPackage,
-        filename: finalFilename,
+        latestVersion: apkVersion,
+        packageName: apkPackage,
+        downloadPath: finalFilename,
         friendlyName,
     };
 }
@@ -71,16 +70,16 @@ async function downloadApk(codename, friendlyName, url, localPath = undefined) {
         "ExpressVPN",
         "https://www.expressvpn.com/latest/android",
     );
-    const v2rayng = await downloadApk(
-        "v2rayng",
-        "v2rayNG",
-        {
-            "arm64-v8a": "https://github.com/2dust/v2rayNG/releases/download/1.8.5/v2rayNG_1.8.5_arm64-v8a.apk",
-            "armeabi-v7a": "https://github.com/2dust/v2rayNG/releases/download/1.8.5/v2rayNG_1.8.5_armeabi-v7a.apk",
-            "x86": "https://github.com/2dust/v2rayNG/releases/download/1.8.5/v2rayNG_1.8.5_x86.apk",
-            "x86_64": "https://github.com/2dust/v2rayNG/releases/download/1.8.5/v2rayNG_1.8.5_x86_64.apk",
-        },
-    );
+    // const v2rayng = await downloadApk(
+    //     "v2rayng",
+    //     "v2rayNG",
+    //     {
+    //         "arm64-v8a": "https://github.com/2dust/v2rayNG/releases/download/1.8.5/v2rayNG_1.8.5_arm64-v8a.apk",
+    //         "armeabi-v7a": "https://github.com/2dust/v2rayNG/releases/download/1.8.5/v2rayNG_1.8.5_armeabi-v7a.apk",
+    //         "x86": "https://github.com/2dust/v2rayNG/releases/download/1.8.5/v2rayNG_1.8.5_x86.apk",
+    //         "x86_64": "https://github.com/2dust/v2rayNG/releases/download/1.8.5/v2rayNG_1.8.5_x86_64.apk",
+    //     },
+    // );
     const ladderAssist = await downloadApk(
         "ladder-assist",
         "梯子辅助",
@@ -89,34 +88,10 @@ async function downloadApk(codename, friendlyName, url, localPath = undefined) {
     );
 
     fs.rmSync(dataPath, { recursive: true, force: true });
-    const finalJson = {
-        random: Math.random().toString().substring(2),
-        apps: [
-            {
-                name: expressVpn.friendlyName,
-                package: expressVpn.package,
-                version: expressVpn.version,
-                filename: expressVpn.filename,
-            },
-            {
-                name: v2rayng.friendlyName,
-                package: v2rayng.package,
-                version: v2rayng.version,
-                filename: v2rayng.filename,
-            },
-            {
-                name: ladderAssist.friendlyName,
-                package: ladderAssist.package,
-                version: ladderAssist.version,
-                filename: ladderAssist.filename,
-            },
-        ],
-        deleteApps: [
-            "com.getsurfboard",
-            "com.github.kr328.clash",
-            "com.github.shadowsocks",
-        ],
-    };
+    const finalJson = [
+        expressVpn,
+        ladderAssist,
+    ];
     await fs.writeFileSync(dataPath, JSON.stringify(finalJson), "utf8");
     console.log("Finished writing data JSON.");
     fs.rmSync(cachePath, { recursive: true, force: true });
